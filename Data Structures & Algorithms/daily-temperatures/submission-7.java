f31029d3337fc97727d749aac3d131d508803dee
Asmitha <asmithagottumukkala1@gmail.com>
2026-09-03 16:44:59 +0530

    Add: daily-temperatures - submission-7

---
+++ Data Structures & Algorithms/daily-temperatures/submission-7.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+       Stack<Integer> st=new Stack<>();
+       int n=temperatures.length;
+       int [] ans=new int[n];
+        for(int i=0;i<n;i++){
+            while(!st.isEmpty()&& temperatures[i]>temperatures[st.peek()]){
+                int index=st.pop();
+                ans[index]=i-index;
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+}
